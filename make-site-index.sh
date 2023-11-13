b7c5cdc308a1bd8e6fa2af859da4d09a21a2771f
Дамјан Георгиевски <gdamjan@gmail.com>
2023-11-13 04:36:42 +0100

    improve site index slides

--- make-site-index.sh
+++ make-site-index.sh
@@ -4,15 +4,21 @@ set -eu
 
 DEST=$1
 
+# install dependencies (revealjs) in $DEST
 pnpm install --prod --modules-dir $DEST/node_modules --virtual-store-dir $DEST/node_modules/.pnpm
 
-echo -e "# start" > $DEST/README.md
+# now create a reveal presentation listing all the presentations
+echo "### Some presentations I've done thru the years…" > $DEST/README.md
+
 for f in [!_]*/index.html; do
   d=`dirname $f`
   cp -r $d -t $DEST/
-  echo -e "## [$d](./$d/index.html)\n---" >> $DEST/README.md
+  echo "---" >> $DEST/README.md
+  echo "## [$d](./$d/index.html)" >> $DEST/README.md
 done
-echo -e "# end." >> $DEST/README.md
 
-cp favicon.ico $DEST/
-cp index.html $DEST/
+# last slide
+echo "---"  >> $DEST/README.md
+echo "# end." >> $DEST/README.md
+
+cp index.html favicon.ico -t $DEST/
